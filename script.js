a43b25213300e1f80b517db26a7bcc4c148b43e0
studiosaudeebelezaalineborges-pixel <studio.saudeebeleza.alineborges@gmail.com>
2026-01-07 15:57:08 -0300

    Create script.js
    Signed-off-by: studiosaudeebelezaalineborges-pixel <studio.saudeebeleza.alineborges@gmail.com>

---
+++ script.js
@@ -0,0 +1,6 @@
+// Exemplo de botão interativo no hero
+const heroButton = document.getElementById('heroButton');
+
+heroButton.addEventListener('click', () => {
+    alert('Bem-vindo(a) ao site da Aline Borges!');
+});
